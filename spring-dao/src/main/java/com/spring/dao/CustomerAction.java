49760356ef7300279c113f7c8dafe08094a12c79
liujijun <liujijun@zcreate.com.cn>
2018-12-11 14:36:21 +0800

    12.11: bug fixed

--- spring-dao/src/main/java/com/spring/dao/CustomerAction.java
+++ spring-dao/src/main/java/com/spring/dao/CustomerAction.java
@@ -1,6 +1,7 @@
 package com.spring.dao;
 
 import com.spring.dao.common.CustomerDaoImpl;
+import com.spring.dao.common.JdbcSupportExm;
 import com.spring.dao.customer.Customer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -9,8 +10,11 @@ public class CustomerAction {
     public static void main(String[] args) {
         ApplicationContext app = new ClassPathXmlApplicationContext("customer.xml");
         CustomerDaoImpl cdi = (CustomerDaoImpl) app.getBean("customerBean");
-        Customer cust = new Customer(2,"sam",26);
+        JdbcSupportExm jse = (JdbcSupportExm) app.getBean("jdbcSupport");
+        Customer cust = new Customer(4,"Liyam",28);
         //cdi.insert(cust);
+        //cdi.insertByTemplate(cust);
+        jse.insert(cust);
         System.out.println("success!");
         Customer result = cdi.findById(2);
         System.out.println(result.getName());
